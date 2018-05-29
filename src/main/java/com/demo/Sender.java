5ee79ccb9e290bb5c5069f1a03c37599cf9c6b1f
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-29 18:03:24 +0800

    Setup sender connection and channel

--- src/main/java/com/demo/Sender.java
+++ src/main/java/com/demo/Sender.java
@@ -1,5 +1,18 @@
 package com.demo;
 
-public class Sender {
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
 
+public class Sender {
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args)
+      throws java.io.IOException, java.util.concurrent.TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+  }
 }
