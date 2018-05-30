60e1e9d096ed5a11446106a05095b38a23427a87
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-30 16:51:19 +0800

    [publish-subscribe] Implement LogReceiver

---
+++ src/main/java/publish_subscribe/LogReceiver.java
@@ -0,0 +1,33 @@
+package publish_subscribe;
+
+import com.rabbitmq.client.*;
+
+public class LogReceiver {
+  private static final String EXCHANGE_NAME = "logs";
+
+  public static void main(String[] args)
+      throws java.io.IOException, java.util.concurrent.TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    // idempotent, just in case Emitter hasn't declare it
+    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+    String queueName = channel.queueDeclare().getQueue();
+    channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+          throws java.io.IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+
+    channel.basicConsume(queueName, true, consumer);
+  }
+}
