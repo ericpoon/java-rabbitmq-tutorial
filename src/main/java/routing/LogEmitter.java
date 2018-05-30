686fec693b60d2d16f7a37bd2432e35de5190a82
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-30 17:20:48 +0800

    [routing] Implement both log emitter and receiver

---
+++ src/main/java/routing/LogEmitter.java
@@ -0,0 +1,51 @@
+package routing;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class LogEmitter {
+  private static final String EXCHANGE_NAME = "multi_type_logs";
+
+  public static void main(String[] args)
+      throws java.io.IOException, java.util.concurrent.TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+    String message = getMessage(args);
+    String severity = getSeverity(args);
+
+    channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8")); // use severity as routing key
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+
+  private static String getMessage(String[] strings) {
+    if (strings.length <= 1) {
+      return "[empty message]";
+    }
+    return joinStrings(strings, " ", 1);
+  }
+
+  private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+    int length = strings.length;
+    if (length == 0) return "";
+    StringBuilder words = new StringBuilder(strings[startIndex]);
+    for (int i = startIndex + 1; i < length; i++) {
+      words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
+  }
+
+  private static String getSeverity(String[] strings) {
+    if (strings.length < 1) return "info";
+    return strings[0];
+  }
+
+}
---
+++ src/main/java/routing/LogReceiver.java
@@ -0,0 +1,42 @@
+package routing;
+
+import com.rabbitmq.client.*;
+
+public class LogReceiver {
+  private static final String EXCHANGE_NAME = "multi_type_logs";
+
+  public static void main(String[] args)
+      throws java.io.IOException, java.util.concurrent.TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    // idempotent, just in case Emitter hasn't declare it
+    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+    String queueName = channel.queueDeclare().getQueue();
+
+    if (args.length == 0){
+      System.err.println("Usage: LogReceiver [info] [warning] [error]");
+      System.exit(1);
+    }
+
+    for (String severity : args) {
+      channel.queueBind(queueName, EXCHANGE_NAME, severity); // use severity as binding key
+      System.out.println(" [*] Will receive log with severity level: " + severity);
+    }
+
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+          throws java.io.IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+
+    channel.basicConsume(queueName, true, consumer);
+  }
+}
