33067d603a618e976f8e3a9e9e2c701629330dea
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-30 16:30:58 +0800

    [publish-subscribe] Implement LogEmitter

---
+++ src/main/java/publish_subscribe/LogEmitter.java
@@ -0,0 +1,45 @@
+package publish_subscribe;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class LogEmitter {
+  private static final String EXCHANGE_NAME = "logs";
+
+  public static void main(String[] args)
+      throws java.io.IOException, java.util.concurrent.TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+
+    String message = getMessage(args);
+
+    channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+
+  private static String getMessage(String[] strings) {
+    if (strings.length == 0) {
+      return "[info] Hello World!";
+    }
+    return joinStrings(strings, " ");
+  }
+
+  private static String joinStrings(String[] strings, String delimiter) {
+    int length = strings.length;
+    if (length == 0) return "";
+    StringBuilder words = new StringBuilder(strings[0]);
+    for (int i = 1; i < length; i++) {
+      words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
+  }
+
+}
